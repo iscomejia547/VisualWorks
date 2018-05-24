bff396c94ff718f7ac8f29744bc0b37f737524f9
Jerry @Win <jf_mejiar547@live.com>
2018-05-24 15:42:37 -0600

    Added hide and seek functionality

--- PairGame/Form1.cs
+++ PairGame/Form1.cs
@@ -12,9 +12,40 @@ namespace PairGame
 {
     public partial class Form1 : Form
     {
+        private Random rnd = new Random();
+        private List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
         public Form1()
         {
             InitializeComponent();
+            loadIcons();
+        }
+        private void loadIcons()
+        {
+            foreach (Control item in GameTab.Controls)
+            {
+                Label lbl = item as Label;
+                if (lbl != null)
+                {
+                    int num = rnd.Next(icons.Count);
+                    lbl.Text = icons[num];
+                    lbl.ForeColor = lbl.BackColor;
+                    icons.RemoveAt(num);
+                }
+            }
+        }
+
+        private void label_clicked(object sender, EventArgs e)
+        {
+            Label clicked = sender as Label;
+            if (clicked != null)
+            {
+                if (clicked.ForeColor == Color.Black) return;
+                clicked.ForeColor = Color.Black;
+            }
         }
     }
 }
